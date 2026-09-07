a0dacde6cfa23bc213cb34b9d149e6fc49be8077
Ozodbek <ozodbekruzimboev5@gmail.com>
2026-09-07 23:36:27 +0400

    Initial commmit

---
+++ src/main/java/com/ozodbek/classifieds/ClassifiedsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ozodbek.classifieds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassifiedsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassifiedsPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozodbek/classifieds/ClassifiedsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozodbek.classifieds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassifiedsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
